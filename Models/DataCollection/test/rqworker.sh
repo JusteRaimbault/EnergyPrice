2a900e6585dc73bd0c139fe7171361ef3e0469ca
JusteRaimbault <juste.raimbault@polytechnique.edu>
2016-09-26 22:33:48 +0200

    test_all

--- Models/DataCollection/test/rqworker.sh
+++ Models/DataCollection/test/rqworker.sh
@@ -1,13 +1,14 @@
 
-TASKID=$1
+TASKNUM=$1
+TASKID=$2
 
-idmax=100
-outfile="data/test.csv"
+idmax=192100
+outfile="data/test_all.csv"
 port=$((9050 + $TASKID))
 
 start=`date +'%s'`
 
-for i in `seq $TASKID $idmax`
+for i in `seq $(($TASKID + 1)) $TASKNUM $idmax`
 do
   curl -s --socks5-hostname 127.0.0.1:$port  www.gasbuddy.com/Station/$i > html/tmp$TASKID
   n=`cat html/tmp | grep "price-display credit-price" | wc -l`
