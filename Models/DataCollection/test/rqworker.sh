601835e276140bd8f9e3c69ace9e08fb34f429a8
JusteRaimbault <juste.raimbault@polytechnique.edu>
2017-01-02 11:42:18 +0100

    remote

--- Models/DataCollection/test/rqworker.sh
+++ Models/DataCollection/test/rqworker.sh
@@ -3,7 +3,9 @@ TASKNUM=$1
 TASKID=$2
 
 idmax=192100
-outfile="data/test_all.csv"
+#idmax=200
+outfile="data/test_all_2.csv"
+#outfile="data/test2.csv"
 port=$((9050 + $TASKID))
 
 start=`date +'%s'`
@@ -11,8 +13,8 @@ start=`date +'%s'`
 for i in `seq $(($TASKID + 1)) $TASKNUM $idmax`
 do
   curl -s --socks5-hostname 127.0.0.1:$port  www.gasbuddy.com/Station/$i > html/tmp$TASKID
-  n=`cat html/tmp | grep "price-display credit-price" | wc -l`
-  station=`cat html/tmp | grep "station-address" | wc -l`
+  n=`cat html/tmp$TASKID | grep "price-display credit-price" | wc -l`
+  station=`cat html/tmp$TASKID | grep "station-address" | wc -l`
   #echo "station $i : $n prices ; station : $station"
   echo "$i;$station;$n" >> $outfile
 done
