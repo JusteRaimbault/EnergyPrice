bd138ed9d4ac221d113a4e5e5f58147d4b6b4b5f
JusteRaimbault <juste.raimbault@polytechnique.edu>
2017-02-18 20:43:07 +0100

    .

--- Models/DataCollection/python/killPool.sh
+++ Models/DataCollection/python/killPool.sh
@@ -1,3 +1,3 @@
-ps -ef | grep "tor --SOCKSPort" |grep -v "tor --SOCKSPort 9049" | grep -v grep | awk -F" " '{print "kill -9 "$2}' | sh
-ps -ef | grep "java -jar lib/torpool.jar" | grep -v grep | awk -F" " '{print "kill -9 "$2}' | sh
+ps -ef | grep "tor --SOCKSPort" |grep -v 'tor --SOCKSPort 904[0-9]' | grep -v grep | awk -F" " '{print "kill -9 "$2}' | sh
+ps -ef | grep "java -jar lib/torpool.jar" |grep -v "java -jar lib/torpool.jar 5 9040" |grep -v grep | awk -F" " '{print "kill -9 "$2}' | sh
 
