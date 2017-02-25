018e99ad89422af644f558c2f8f527c448671ce1
JusteRaimbault <raimbaultjwin@gmail.com>
2017-02-25 17:58:48 +0100

    gecocoding_ok

--- Models/DataCollection/python/geocoding.py
+++ Models/DataCollection/python/geocoding.py
@@ -1,39 +1,102 @@
 
-from nominatim import Nominatim
-import utils,functools,traceback
+import functools,traceback,os,sys,time,requests
+#from nominatim import Nominatim
+import utils
 
 # read Nominatim conditions :
 #  https://operations.osmfoundation.org/policies/nominatim/
+# -> must wait 1sec between requests ; needs to use a torpool for efficiency
+#
 
-nom = Nominatim()
+adresses_file = 'loc/adresses.csv'
+locdir = 'loc'
+nports = 5
 
-data = utils.read_csv('test/adresses_sample.csv',';')
+startingport = int(sys.argv[1])
+proxies = []
+for i in range(nports) :
+    proxies.append({'http':'socks5://localhost:'+str(startingport+i)})
+
+
+#nom = Nominatim()
+
+#data = utils.read_csv('test/adresses_sample.csv',';')
+
+
+
+
+def addr_search_query(line,markers):
+    addr=line[1].replace(' N ',' ').replace(' S ',' ').replace(' E ',' ').replace(' W ',' ')
+    pos = []
+    for marker in markers :
+        ind = addr.find(marker)
+        if ind != -1 :
+            pos.append([marker,ind/len(addr)])
+    sorted_pos = sorted(pos, key=lambda entry: entry[1])
+
+    prefix = ''
+    if len(sorted_pos)<2 :
+        prefix = addr
+    else :
+        markerind = 0
+        if sorted_pos[0][1] < 0.3 : markerind = 1
+        prefix = addr.split(sorted_pos[markerind][0])[0]+sorted_pos[markerind][0]
+
+    #return(prefix)
+    return(prefix+' '+line[2]+' '+line[3])
+
+
+markers =  ['St','Rd','Ave','Dr','Blvd','Hwy','Ln','Pkwy','Way']
+
 
 count=0
-for line in data:
-    print('Getting : '+line[1])
-    raw = line[1].split(' ')
-    found = False
-    for i in range(2,len(raw)):
-        #print(raw[0:(i-1)])
-        #print(raw[i:len(raw)-1])
-        str1 = functools.reduce(lambda x, y: x+y,raw[0:(i-1)])+' '+line[2]
-        str2 = functools.reduce(lambda x, y: x+y,raw[i:len(raw)])+' '+line[2]
-        query1 = nom.query(str1)
-        query2 = nom.query(str2)
+j=0
+with open(adresses_file) as f:
+    for linestr in f:
+        line = linestr.replace('\n','').split(';')
+        print('Getting station '+line[0]+' - address : '+line[1]+' '+line[2]+' '+line[3])
+        #raw = line[1].split(' ')
+        querystring = addr_search_query(line,markers)
+        #print(querystring)
+        query = requests.get('http://nominatim.openstreetmap.org/search',params = {'format':'json','q':querystring},proxies=proxies[j%nports])
+        res = query.json()
         try :
-            if len(query1) > 0 :
-                found=True
-                print(str1+" - coords : "+str(query1[0]['lon'])+' ; '+str(query1[0]['lat']))
+            if len(res) > 0 :
+                print(querystring+" - coords : "+str(res[0]['lat'])+' ; '+str(res[0]['lon']));print('')
+                utils.append_csv([[line[0],querystring,str(res[0]['lat']),str(res[0]['lon'])]],locdir+'/coordinates.csv',';')
+                count=count+1
         except Exception :
             print(traceback.format_exc())
-        try :
-            if len(query2) > 0 :
-                found=True
-                print(str2+" - coords : "+str(query2[0]['lon'])+' ; '+str(query2[0]['lat']))
-        except Exception :
-            print(traceback.format_exc())
-    if found :
-        count=count+1
 
-print(count/len(data))
+        j=j+1
+        time.sleep(1.0/nports)
+
+
+
+print(count/j)
+
+
+
+        #for i in range(2,len(raw)):
+            #str1 = functools.reduce(lambda x, y: x+' '+y,raw[0:(i-1)])+' '+line[2]+' '+line[3]+' '+line[4]
+            #str2 = functools.reduce(lambda x, y: x+' '+y,raw[i:len(raw)])+' '+line[2]
+            #query1 = nom.query(str1)
+            #query2 = nom.query(str2)
+
+        #    query1 = requests.get('http://nominatim.openstreetmap.org/search',params = {'format':'json','q':str1})
+    #        res1 = query1.json()
+    #        time.sleep(1.0)
+            #query2 = requests.get('http://nominatim.openstreetmap.org/search',params = {'format':'json','q':str2})
+            #res2 = query2.json()
+            #try :
+            #    if len(res1) > 0 :
+            #        found=True
+            #        print(str1+" - coords : "+str(res1[0]['lat'])+' ; '+str(res1[0]['lon']))
+            #except Exception :
+            #        print(traceback.format_exc())
+            #try :
+            #    if len(res2) > 0 :
+            #        found=True
+            #        print(str2+" - coords : "+str(res2[0]['lon'])+' ; '+str(res2[0]['lat']))
+            #except Exception :
+            #    print(traceback.format_exc())
